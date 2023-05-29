849bbdbb8dac18652abae0f25b6ac6a58e6837ae
knox <knoxfighter@asdff45.de>
2023-05-30 00:41:23 +0200

    added go module && added docker image building && added authentication && added creation of hashes on upload

---
+++ login.gtpl
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<form enctype="multipart/form-data" action="/login" method="post">
+	<input type="text" name="username" placeholder="Username" />
+	<input type="password" name="password" placeholder="Password" />
+	<input type="submit" value="login">
+</form>
+</body>
+</html>
--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
-	"crypto/rand"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
+	"github.com/google/uuid"
 	"html/template"
 	"io/ioutil"
 	"log"
-	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
-const maxUploadSize = 2 * 1024 * 1024 // 2 mb
+type Session struct {
+	username string
+	expiry   time.Time
+}
+
+var users map[string]string
+
+var sessions map[string]Session
+
+func (s *Session) isExpired() bool {
+	return s.expiry.Before(time.Now())
+}
+
+func init() {
+	users = make(map[string]string)
+	sessions = make(map[string]Session)
+	users["asdff45"] = "123456"
+}
+
+const maxUploadSize = 2 * 1024 * 1024 * 1024 // 2 Gb
 var uploadPath = os.TempDir()
 
 func main() {
+	users[os.Getenv("username")] = os.Getenv("password")
+
 	http.HandleFunc("/upload", uploadFileHandler)
+	http.HandleFunc("/login", loginHandler)
 
 	fs := http.FileServer(http.Dir(uploadPath))
 	http.Handle("/files/", http.StripPrefix("/files", fs))
@@ -25,7 +51,82 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func checkSession(w http.ResponseWriter, r *http.Request) (success bool) {
+	success = false
+
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			// If the cookie is not set, return an unauthorized status
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		// For any other type of error, return a bad request status
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	sessionToken := cookie.Value
+
+	session, ok := sessions[sessionToken]
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	if session.isExpired() {
+		delete(sessions, sessionToken)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	return true
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+		return
+	}
+
+	err := r.ParseMultipartForm(10 * 1024) // 1kB
+	if err != nil {
+		log.Println("CANT_PARSE_FORM")
+		renderError(w, "CANT_PARSE_FORM", http.StatusInternalServerError)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	actualPassword, ok := users[username]
+
+	if !ok || actualPassword != password {
+		log.Println("wrong password")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	token := uuid.NewString()
+	expires := time.Now().Add(120 * time.Second)
+
+	sessions[token] = Session{
+		username: username,
+		expiry:   expires,
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   token,
+		Expires: expires,
+	})
+}
+
 func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	if !checkSession(w, r) {
+		return
+	}
+
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("upload.gtpl")
 		t.Execute(w, nil)
@@ -44,41 +145,32 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer file.Close()
+
 	// Get and print out file size
 	fileSize := fileHeader.Size
 	fmt.Printf("File size (bytes): %v\n", fileSize)
+
 	// validate file size
 	if fileSize > maxUploadSize {
 		renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
 		return
 	}
+
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		renderError(w, "INVALID_FILE", http.StatusBadRequest)
 		return
 	}
 
-	// check file type, detectcontenttype only needs the first 512 bytes
-	detectedFileType := http.DetectContentType(fileBytes)
-	switch detectedFileType {
-	case "image/jpeg", "image/jpg":
-	case "image/gif", "image/png":
-	case "application/pdf":
-		break
-	default:
+	// check file extension, we don't allow hash files
+	fileExtension := filepath.Ext(fileHeader.Filename)
+
+	if fileExtension == ".md5" || fileExtension == "sha1" || fileExtension == "sha256" || fileExtension == "sha512" {
 		renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
 		return
 	}
-	fileName := randToken(12)
-	fileEndings, err := mime.ExtensionsByType(detectedFileType)
-	if err != nil {
-		renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
-		return
-	}
 
-	newFileName := fileName + fileEndings[0]
-	newPath := filepath.Join(uploadPath, newFileName)
-	fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
+	newPath := filepath.Join(uploadPath, fileHeader.Filename)
 
 	// write file
 	newFile, err := os.Create(newPath)
@@ -87,20 +179,43 @@ func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer newFile.Close() // idempotent, okay to call twice
+
 	if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
 		renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
 		return
 	}
-	w.Write([]byte(fmt.Sprintf("SUCCESS - use /files/%v to access the file", newFileName)))
+
+	// create hash files for uploaded file
+	md5Hash := md5.Sum(fileBytes)
+	sha1Hash := sha1.Sum(fileBytes)
+	sha256Hash := sha256.Sum256(fileBytes)
+	sha512Hash := sha512.Sum512(fileBytes)
+
+	err = ioutil.WriteFile(fileHeader.Filename+".md5", md5Hash[:], 0644)
+	if err != nil {
+		renderError(w, "CANT_WRITE_MD5", http.StatusInternalServerError)
+		return
+	}
+	err = ioutil.WriteFile(fileHeader.Filename+".sha1", sha1Hash[:], 0644)
+	if err != nil {
+		renderError(w, "CANT_WRITE_SHA1", http.StatusInternalServerError)
+		return
+	}
+	err = ioutil.WriteFile(fileHeader.Filename+".sha256", sha256Hash[:], 0644)
+	if err != nil {
+		renderError(w, "CANT_WRITE_SHA256", http.StatusInternalServerError)
+		return
+	}
+	err = ioutil.WriteFile(fileHeader.Filename+".sha512", sha512Hash[:], 0644)
+	if err != nil {
+		renderError(w, "CANT_WRITE_SHA512", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("SUCCESS - use /files/%v to access the file", fileHeader.Filename)))
 }
 
 func renderError(w http.ResponseWriter, message string, statusCode int) {
 	w.WriteHeader(statusCode)
 	w.Write([]byte(message))
-}
-
-func randToken(len int) string {
-	b := make([]byte, len)
-	rand.Read(b)
-	return fmt.Sprintf("%x", b)
 }
